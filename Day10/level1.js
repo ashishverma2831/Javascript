c2a8d620dce09253ff26e424ae2852f09b95a51c
Ashish Verma <ashishverma510207@gmail.com>
2023-12-14 16:54:42 +0530

    day10 completed

---
+++ Day10/level1.js
@@ -0,0 +1,24 @@
+// const a = new Set();
+// a.add(10);
+// a.add(20);
+// a.add(30);
+// a.add(40);
+// a.add(50);
+// // a.clear()
+// a.delete(20)
+// console.log(a);
+
+let a = [1, 2, 3, 4, 5]
+let b = [3, 4, 5, 6]
+
+let c = [...a, ...b]
+let d = a.filter((num) => B.has(num))
+let e = a.filter((num) => !B.has(num))
+
+let A = new Set(a)
+let B = new Set(b)
+let C = new Set(c)
+
+console.log(C)
+
+const z = new Map();
