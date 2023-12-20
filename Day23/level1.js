c051a36df418ffbb1ffc8306924abafed534cfc0
Ashish Verma <ashishverma510207@gmail.com>
2023-12-20 21:35:04 +0530

    no work

---
+++ Day23/level1.js
@@ -0,0 +1,8 @@
+// selectedElement.addEventListener('eventlistner', function(e) {
+//     // the activity you want to occur after the event will be in here
+//   })
+//   // or
+  
+//   selectedElement.addEventListener('eventlistner', e => {
+//     // the activity you want to occur after the event will be in here
+//   })
